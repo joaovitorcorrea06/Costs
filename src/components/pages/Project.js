2f7ee03bb35437acc00c83373e3997393d5fec61
joao vitor correa <joaov1997@hotmail.com>
2022-07-28 17:54:35 -0300

    .

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,8 +1,40 @@
 import styles from './Project.module.css'
 
+import {useParams} from 'react-router-dom'
+import { useEffect, useState } from 'react';
+
+import Loading from '../layout/Loading'
+
 function Project(){
+
+    const {id} = useParams()
+
+    const [project, setProject] = useState([])
+
+    useEffect(() => {
+          
+        fetch(`http://localhost:5000/projects/${id}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+        })
+        .then( (resp) => resp.json())
+        .then( (data) =>{
+            setProject(data)
+        })
+        .catch(err => console.log(err))
+    }, [id]);
+
+
     return(
-        <p>projeto</p>
+        <>
+        {project.name ? (
+            <p>{project.name}</p>
+        ):(
+            <Loading/>
+        )}
+        </>
     )
 }
 
